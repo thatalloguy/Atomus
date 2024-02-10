f1ff4766aff86e4c9210e9eb1b22dc5af2747664
thatalloguy <allosander@gmail.com>
2024-02-10 21:51:23 +0100

    IM MOTIVATED AGAIN RAHHHHH

--- Renderer/VkEngine.h
+++ Renderer/VkEngine.h
@@ -28,6 +28,9 @@ struct FrameData {
 
     VkCommandPool _commandPool;
     VkCommandBuffer _mainCommandBuffer;
+
+    VkSemaphore _swapchainSemaphore, _renderSemaphore;
+    VkFence _renderFence;
 };
 
 constexpr unsigned int FRAME_OVERLAP = 2;
