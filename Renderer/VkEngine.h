20781abd36bb9e10e4de0a632be1c3e446dbf616
thatalloguy <allosander@gmail.com>
2024-02-10 11:20:54 +0100

    Finalized Thirdparty libs + started with vulkan engine structure

---
+++ Renderer/VkEngine.h
@@ -0,0 +1,20 @@
+//
+// Created by allo on 2/10/2024.
+
+/*
+ * Purpose of this file (for future reference):
+ * -This will be the main class for the (render) engine, and where most of the code will go.
+ */
+
+
+
+#ifndef ATOMUSVULKAN_VKENGINE_H
+#define ATOMUSVULKAN_VKENGINE_H
+
+
+class VkEngine {
+
+};
+
+
+#endif //ATOMUSVULKAN_VKENGINE_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <VkBootstrap.h>
 #include <vulkan/vulkan.h>
-#include <SDL2/SDL_vulkan.h>
+#include <spdlog/spdlog.h>
+#include <imgui.h>
+#include <stb_image.h>
+#include <fastgltf/core.hpp>
+#include <SDL_vulkan.h>
 
 
 int main() {
