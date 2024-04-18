a7075d48460bace675ee8f9d4c5e64c6df575497
thatalloguy <allosander@gmail.com>
2024-04-18 11:47:12 +0200

    DescriptorWriter

--- Renderer/VkDescriptors.cpp
+++ Renderer/VkDescriptors.cpp
@@ -184,3 +184,61 @@ VkDescriptorPool DescriptorAllocatorGrowable::createPool(VkDevice device, uint32
     VK_CHECK(vkCreateDescriptorPool(device, &poolInfo, nullptr, &newPool));
     return newPool;
 }
+
+
+
+
+
+
+
+
+
+void DescriptorWriter::writeImage(int binding, VkImageView image, VkSampler sampler, VkImageLayout layout, VkDescriptorType type) {
+    VkDescriptorImageInfo& info = imageInfos.emplace_back(VkDescriptorImageInfo{
+        .sampler = sampler,
+        .imageView = image,
+        .imageLayout = layout
+    });
+
+    VkWriteDescriptorSet write = { .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET };
+
+    write.dstBinding = binding;
+    write.dstSet = VK_NULL_HANDLE;
+    write.descriptorCount = 1;
+    write.descriptorType = type;
+    write.pImageInfo = &info;
+
+    writes.push_back(write);
+}
+
+void DescriptorWriter::writeBuffer(int binding, VkBuffer buffer, size_t size, size_t offset, VkDescriptorType type) {
+    VkDescriptorBufferInfo& info = bufferInfos.emplace_back(VkDescriptorBufferInfo{
+            .buffer = buffer,
+            .offset = offset,
+            .range = size
+    });
+
+    VkWriteDescriptorSet write = {.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET };
+
+    write.dstBinding = binding;
+    write.dstSet = VK_NULL_HANDLE; // left empty for now
+    write.descriptorCount = 1;
+    write.descriptorType = type;
+    write.pBufferInfo = &info;
+
+    writes.push_back(write);
+}
+
+void DescriptorWriter::clear() {
+    imageInfos.clear();
+    writes.clear();
+    bufferInfos.clear();
+}
+
+void DescriptorWriter::updateSet(VkDevice device, VkDescriptorSet set) {
+    for (VkWriteDescriptorSet& write : writes) {
+        write.dstSet = set;
+    }
+
+    vkUpdateDescriptorSets(device, (uint32_t)writes.size(), writes.data(), 0, nullptr);
+}
--- Renderer/VkDescriptors.h
+++ Renderer/VkDescriptors.h
@@ -67,5 +67,16 @@ private:
 
 };
 
+struct DescriptorWriter {
+    std::deque<VkDescriptorImageInfo> imageInfos;
+    std::deque<VkDescriptorBufferInfo> bufferInfos;
+    std::vector<VkWriteDescriptorSet> writes;
+
+    void writeImage(int binding, VkImageView image, VkSampler sampler, VkImageLayout layout, VkDescriptorType type);
+    void writeBuffer(int binding, VkBuffer buffer, size_t size, size_t offset, VkDescriptorType type);
+
+    void clear();
+    void updateSet(VkDevice device, VkDescriptorSet set);
+};
 
 #endif //ATOMUSVULKAN_VKDESCRIPTORS_H
